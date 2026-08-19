1a79d8bfea80af7b10b689a736fbd2679696ece2
Luca Dinies <ldinies4498@gmail.com>
2026-08-19 08:22:19 +0200

    Revert "Normalize small tags in headline HTML"
    This reverts commit 2052ed522dcf713a291103755c824eb21eaccb35.

--- src/wwElement.vue
+++ src/wwElement.vue
@@ -199,13 +199,6 @@ function sanitizeHtml(value) {
     .replace(/javascript:/gi, '');
 }
 
-function normalizeHeadlineHtml(value) {
-  return sanitizeHtml(value)
-    .replace(/<small(\s[^>]*)?>/gi, '<span class="quiz-headline-small" style="font-size:0.72em;font-weight:inherit;line-height:inherit;">')
-    .replace(/<\/small>/gi, '</span>')
-    .replace(/\n/g, '<br>');
-}
-
 function normalizeList(value) {
   const list = parseMaybeJson(value, []);
   if (!Array.isArray(list)) return [];
@@ -293,7 +286,7 @@ export default {
       }
       return `Neugierig, warum sich ein Wechsel als ${positionName} lohnt?`;
     });
-    const headlineHtml = computed(() => normalizeHeadlineHtml(headline.value));
+    const headlineHtml = computed(() => sanitizeHtml(headline.value).replace(/\n/g, '<br>'));
     const eyebrow = computed(() => text(content.value.eyebrow));
     const locationDisplay = computed(() => text(content.value.locationDisplay) || text(quiz.value.location_display));
     const mainBenefit = computed(() => text(quiz.value.main_benefit));
@@ -481,8 +474,7 @@ export default {
   line-height: 1.14;
 }
 
-.quiz-block__headline :deep(small),
-.quiz-block__headline :deep(.quiz-headline-small) {
+.quiz-block__headline :deep(small) {
   font-size: 0.72em;
   font-weight: inherit;
   line-height: inherit;
